5a5666cb7665165c6c7701fee0c37492330bcfa9
JR <z8ruan@uwaterloo.ca>
2023-06-01 23:35:32 -0400

    rabbitmq

--- producer/src/main/java/org/example/HelloWorld.java
+++ producer/src/main/java/org/example/HelloWorld.java
@@ -1,9 +1,26 @@
 package org.example;
 
-import sun.jvmstat.perfdata.monitor.PerfStringVariableMonitor;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+
+
 
 public class HelloWorld {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory=new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(5672);
+        //factory.setVirtualHost("/itcast");
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        Connection connection=factory.newConnection();
+        Channel channel=connection.createChannel();
+        channel.queueDeclare("hello_world",true,false,false,null);
+        String body="hello rabbitmq";
+        channel.basicPublish("","hello_world",null,body.getBytes());
+        channel.close();
+        connection.close();
     }
 }
